edcddf8b6169d935b3fd6eab5619dd8b4140f7a0
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2023-04-27 18:18:44 +0530

    init project w tailwind

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-xl bg-red-300">Hello World</h1>
+    </>
+  );
+}
+
+export default App;
