f6c4f94d244bf16f1aa5bf288155b477346cb8a5
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2023-04-28 14:52:03 +0530

    add flip state to component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,7 +1,19 @@
+import { useState } from "react";
+
 const Card: React.FC<{ content: string }> = ({ content }) => {
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  const cardStyling =
+    "border-red-500  border-2 bg-white rounded-md aspect-[3/4] w-32 flex flex-col items-center justify-center";
+
   return (
-    <div className="border-red-500 border-2 bg-white rounded-md aspect-[3/4] w-32 flex flex-col items-center justify-center">
-      <div className="text-5xl mx-auto w-fit">{content}</div>
+    <div
+      onClick={() => {
+        setIsFlipped(!isFlipped);
+      }}
+      className={cardStyling + " " + (isFlipped ? "bg-red-500" : "")}
+    >
+      {!isFlipped && <div className="text-5xl mx-auto w-fit">{content}</div>}
     </div>
   );
 };
